51dc1473048b0afd02d8922279259c4ad6ef9ef1
segunojo1 <olusegunkd2017@gmail.com>
2024-03-25 00:00:52 +0100

    increase doc

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""initialization of the modellll"""
+"""initialization of the modellll which is meant for the file storage stuff"""
 # from libs import get_classes, classes
 from .engine.file_storage import FileStorage
 
