0f6163c5789d51da2606ca5a7ce05cd8cf753aeb
segunojo1 <olusegunkd2017@gmail.com>
2024-03-25 00:44:22 +0100

    state

--- models/state.py
+++ models/state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """the STATE model created now"""
+from models.base_model import BaseModel
 
 
 class State(BaseModel):
     """state model that ingherit from basemodel regvejk"""
-    
+    name = ""
