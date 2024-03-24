158655c4003a93e4268e37484836301733ca1769
segunojo1 <olusegunkd2017@gmail.com>
2024-03-25 00:43:26 +0100

    usr

---
+++ models/state.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""the STATE model created now"""
+
+
+class State(BaseModel):
+    """state model that ingherit from basemodel regvejk"""
+    
--- models/user.py
+++ models/user.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """the user model created now"""
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """user class that inherits from the base model"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get('email', "")
-        self.password = kwargs.get('password', "")
-        self.first_name = kwargs.get('first_name', "")
-        self.last_name = kwargs.get('last_name', "")
-
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    first_name = ""
+    last_name = ""
+    email = ""
+    password = ""
