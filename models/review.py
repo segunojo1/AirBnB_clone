bc1cd51a4f021eb364410ff633f878e4d3467dc1
segunojo1 <olusegunkd2017@gmail.com>
2024-03-25 00:49:10 +0100

    review

---
+++ models/review.py
@@ -0,0 +1,9 @@
+from .base_model import BaseModel
+"""the STATE model created now"""
+
+
+class Review(BaseModel):
+    """state model that ingherit from basemodel regvejk"""
+    place_id = ""
+    user_id = ""
+    text = ""
