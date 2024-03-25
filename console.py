361c48ef9d7ab955b8ecbdbf4e855d3ddc9aaa98
segunojo1 <olusegunkd2017@gmail.com>
2024-03-25 19:18:48 +0100

    console.py

--- console.py
+++ console.py
@@ -28,12 +28,52 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """This helps in the running the create command"""
-        print(model)
-        if not self.checker(model, ["n", 'ec']):
+        print(arg)
+        if not self.checker(arg, ["n", 'ec']):
             return
-        ins = classes[model]()
-        ins.save()
-        print(ins.id)
+        bss = classes[arg]()
+        bss.save()
+        print(bss.id)
+
+    def do_show(self, model):
+        """show command to display information about a specific instance"""
+
+        if not self.checker(model, ["n", "l", "ec", "es"]):
+            return
+        cls, key = model.split()
+        print(classes[cls].show(key))
+
+    def do_destroy(self, model):
+        """destroy command to remove a specific instance"""
+
+        if not self.checker(model, ["n", "l", "ec", "es"]):
+            return
+        cls, key = model.split()
+        classes[cls].destroy(key)
+
+    def do_all(self, model):
+        """
+        all command to display all instances of a specific model or
+        all instances across all models if none is
+        specified
+        """
+
+        if model and not self.checker(model, ["ec"]):
+            return
+        if model:
+            print(classes[model].all())
+        else:
+            print(storage.get_all())
+
+    def do_update(self, model):
+        """update command to modify an instance attribute value"""
+
+        keys = ["n", "l", "ec", "es", "a", "v"]
+        if not self.checker(model, keys):
+            return
+        extras = model.split()
+        classes[extras[0]].update(extras[1], extras[2], extras[3])
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
---
+++ models/amenity.py
@@ -0,0 +1,7 @@
+from .base_model import BaseModel
+"""the amenity model created now"""
+
+
+class Amenity(BaseModel):
+    """amenity model that ingherit from basemodel regvejk"""
+    name = ""
