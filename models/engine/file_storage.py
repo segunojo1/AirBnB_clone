a79e419599dfdbc7cbf87187d855a143a7b6a4dd
segunojo1 <olusegunkd2017@gmail.com>
2024-03-25 13:44:34 +0100

    self

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,7 +37,7 @@ class FileStorage:
         reload deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
         """
         if os.path.isFile(FileStorage.__file_path):
-            with open(self.__file_path, "r", encoding="utf-8") as rl:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as rl:
                 try:
                     des = json.load(rl)
                     for key, value in des.items():
