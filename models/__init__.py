2dd487829dd169bb7f3cbe8c2de2a05aadf64473
segunojo1 <olusegunkd2017@gmail.com>
2024-03-25 13:36:25 +0100

    remove lib

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """initialization of the modellll which is meant for the file storage stuff"""
-from libs import get_classes, classes
+# from libs import get_classes, classes
 from .engine.file_storage import FileStorage
 
 # __all__ = ("classes", "storage")
