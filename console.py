f72928b3e8e5b9862cf02a89ecca97753f619c04
segunojo1 <olusegunkd2017@gmail.com>
2024-03-26 22:16:42 +0100

    update console

--- console.py
+++ console.py
@@ -36,43 +36,26 @@ class HBNBCommand(cmd.Cmd):
         print(bss.id)
 
     def do_show(self, model):
-        """show command to display information about a specific instance"""
+        """this method allows the show command to work"""
 
-        if not self.checker(model, ["n", "l", "ec", "es"]):
-            return
-        cls, key = model.split()
-        print(classes[cls].show(key))
+        
 
     def do_destroy(self, model):
-        """destroy command to remove a specific instance"""
+        """this method allows the destroy command to remove an instance"""
 
-        if not self.checker(model, ["n", "l", "ec", "es"]):
-            return
-        cls, key = model.split()
-        classes[cls].destroy(key)
+        
 
     def do_all(self, model):
         """
-        all command to display all instances of a specific model or
-        all instances across all models if none is
-        specified
+        this method allows you dislay all instances of the model
         """
 
-        if model and not self.checker(model, ["ec"]):
-            return
-        if model:
-            print(classes[model].all())
-        else:
-            print(storage.get_all())
+        
 
     def do_update(self, model):
-        """update command to modify an instance attribute value"""
+        """this command allows you to updatean instance"""
 
-        keys = ["n", "l", "ec", "es", "a", "v"]
-        if not self.checker(model, keys):
-            return
-        extras = model.split()
-        classes[extras[0]].update(extras[1], extras[2], extras[3])
+        
 
 
 if __name__ == "__main__":
